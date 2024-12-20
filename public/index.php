51e7a1a3bf973b60b8548b3c869436fbfe01e496
Dellon <jy.dellon@gmail.com>
2024-12-20 06:16:52 +0100

    first commit

--- public/index.php
+++ public/index.php
@@ -2,8 +2,18 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+// Inclure l'autoload généré par Composer
+require_once __DIR__ . '/../vendor/autoload.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+// Vérification des variables d'environnement pour Vercel
+$env = $_SERVER['APP_ENV'] ?? 'prod'; // Par défaut à 'prod'
+$debug = $_SERVER['APP_DEBUG'] ?? ($env !== 'prod');
+
+// Initialisation du Kernel Symfony
+$kernel = new Kernel($env, (bool) $debug);
+$request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
+$response = $kernel->handle($request);
+
+// Envoyer la réponse au client
+$response->send();
+$kernel->terminate($request, $response);

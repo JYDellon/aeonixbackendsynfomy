a50c36b49d81ce7d45f4bff33ca126ae5e344fb5
Dellon <jy.dellon@gmail.com>
2024-12-20 08:42:56 +0100

    maj

--- buid.sh
+++ buid.sh
@@ -1,13 +1,38 @@
+# #!/bin/bash
+
+# # Installer les dépendances PHP
+# composer install --no-dev --optimize-autoloader
+
+# # Effacer et précompiler le cache Symfony
+# php bin/console cache:clear --env=prod
+
+# # Vérifiez si les clés JWT existent
+# if [ ! -f "config/jwt/private.pem" ] || [ ! -f "config/jwt/public.pem" ]; then
+#     echo "Les fichiers JWT ne sont pas présents."
+#     exit 1
+# fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #!/bin/bash
 
-# Installer les dépendances PHP
+# Installer les dépendances avec Composer
 composer install --no-dev --optimize-autoloader
 
-# Effacer et précompiler le cache Symfony
+# Préparer le cache Symfony
 php bin/console cache:clear --env=prod
+php bin/console cache:warmup --env=prod
 
-# Vérifiez si les clés JWT existent
-if [ ! -f "config/jwt/private.pem" ] || [ ! -f "config/jwt/public.pem" ]; then
-    echo "Les fichiers JWT ne sont pas présents."
-    exit 1
-fi
+# Vérifiez les permissions des fichiers et dossiers
+chmod -R 775 var/cache var/log

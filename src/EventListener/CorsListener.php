0c19d59b38b29d3cd8e4552f3ad2e833c2ed6c5c
Dellon <jy.dellon@gmail.com>
2024-12-20 03:20:11 +0100

    ma

--- src/EventListener/CorsListener.php
+++ src/EventListener/CorsListener.php
@@ -9,10 +9,13 @@ class CorsListener
     public function onKernelResponse(ResponseEvent $event): void
     {
         $response = $event->getResponse();
+        $request = $event->getRequest();
 
-        // Ajouter les en-têtes CORS
-        $response->headers->set('Access-Control-Allow-Origin', '*'); // Remplacez '*' par votre domaine si nécessaire
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        // Vérifiez si la route correspond à une API
+        if (strpos($request->getPathInfo(), '/api/') === 0) {
+            $response->headers->set('Access-Control-Allow-Origin', '*'); // Remplacez '*' par votre domaine si nécessaire
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        }
     }
 }

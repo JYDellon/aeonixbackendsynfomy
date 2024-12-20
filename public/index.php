cf43528f0b191eed3a8ea2410949d27e9fe655e1
Dellon <jy.dellon@gmail.com>
2024-12-20 05:48:04 +0100

    first commit

--- public/index.php
+++ public/index.php
@@ -1,11 +1,9 @@
 <?php
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    $_SERVER['APP_CACHE_DIR'] = '/tmp/cache';
-    $_SERVER['APP_LOG_DIR'] = '/tmp/log';
-
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };

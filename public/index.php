7fcc5502754cf8aa9eabe9e2190ff8bd69189b6d
Dellon <jy.dellon@gmail.com>
2024-12-20 03:29:42 +0100

    ma

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php
-
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    // Redirige le cache et les logs vers /tmp
+    $_SERVER['APP_CACHE_DIR'] = '/tmp/cache';
+    $_SERVER['APP_LOG_DIR'] = '/tmp/log';
+
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,14 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getCacheDir(): string
+    {
+        return $_SERVER['APP_CACHE_DIR'] ?? $this->getProjectDir() . '/var/cache/' . $this->environment;
+    }
+
+    public function getLogDir(): string
+    {
+        return $_SERVER['APP_LOG_DIR'] ?? $this->getProjectDir() . '/var/log';
+    }
 }
